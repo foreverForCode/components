6027a686a81382c16db196b12cd0e9c7bdd986ee
lixiang <freewm@163.com>
2022-01-05 09:37:24 +0800

    feat: 添加资源路径

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'z-mobile',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  title: 'kk-mobile',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  publicPath: '/components/',
   themeConfig: {
     hd: {
       // umi-hd 的 750 高清方案（默认值）
@@ -18,7 +21,8 @@ export default defineConfig({
       //   { minWidth: 376, maxWidth: 750, mode: 'vw', options: [100, 1500] },
       // ],
       // 更多 rule 配置访问 https://github.com/umijs/dumi/blob/master/packages/theme-mobile/src/typings/config.d.ts#L7
-    }
-  }
+    },
+  },
+
   // more config: https://d.umijs.org/config
 });
